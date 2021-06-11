162a604e9e3fd86d9dab654d1f4a4b588b599986
xiaosong <xiaosong@xiaosongdeMacBook-Air.local>
2021-06-11 13:17:36 +0800

    优化检查服务器部分

--- shellStript/centos-install-nginx.sh
+++ shellStript/centos-install-nginx.sh
@@ -53,39 +53,6 @@ function server_check () {
     mv /etc/vimrc /tmp/vimrc.bak
     wget https://10mzwnga.com/vimrc.demo -O /etc/vimrc
 
-    # 内核优化
-    # KERNELFILE="/usr/lib/sysctl.d/00-system.conf"
-    # PARAMETER=(
-    #     "fs.file-max = 65535"
-    #     "net.ipv4.tcp_timestamps = 0"
-    #     "net.ipv4.tcp_synack_retries = 5"
-    #     "net.ipv4.tcp_syn_retries = 5"
-    #     "net.ipv4.tcp_tw_recycle = 1"
-    #     "net.ipv4.tcp_tw_reuse = 1"
-    #     "net.ipv4.tcp_fin_timeout = 30"
-    #     "net.ipv4.ip_local_port_range = 1024 65535"
-    #     "kernel.shmall = 2097152"
-    #     "kernel.shmmax = 2147483648"
-    #     "kernel.shmmni = 4096"
-    #     "kernel.sem = 5010 641280 5010 128"
-    #     "net.core.wmem_default=262144"
-    #     "net.core.wmem_max=262144"
-    #     "net.core.rmem_default=4194304"
-    #     "net.core.rmem_max=4194304"
-    #     "net.ipv4.tcp_fin_timeout = 10"
-    #     "net.ipv4.tcp_keepalive_time = 30"
-    #     "net.ipv4.tcp_window_scaling = 0"
-    #     "net.ipv4.tcp_sack = 0"
-    # )
-    # for JUDGMENT in "${PARAMETER[@]}"
-    # do
-    #     sysctl -a | grep "${JUDGMENT}" > /dev/null
-    #     if [ "$?" -ne 0 ];then
-    #         echo "${JUDGMENT}" >> ${KERNELFILE}
-    #     fi
-    # done
-    # sysctl -p
-
     # 检查SELiunx
     GETENFORCE="/usr/sbin/getenforce"
     SELINUX=$(${GETENFORCE})
